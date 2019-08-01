8ada499d8b3cf7b867012b4ebdf5be8e5286af11
Sebastian Bock <s.bock@neusta.de>
2019-08-01 10:47:24 +0200

    changed comment-format and added PHP-Doc for toRoman()-Method

--- php-mocking-kata/src/Calculations/ArabicConverterInterface.php
+++ php-mocking-kata/src/Calculations/ArabicConverterInterface.php
@@ -8,22 +8,26 @@ namespace RealLife\Calculations;
  */
 interface ArabicConverterInterface
 {
-    /// Converts the given arabic number to its roman representation.
-    /// An Exception is thrown when number is less than 1 or greater than 3888.
-    /// 1       I
-    /// 5       V
-    /// 10      X
-    /// 50      L
-    /// 100     C
-    /// 500     D
-    /// 1000    M
-    ///
-    /// Convert numbers from 1 to 3888.
-    ///
-    /// Only I, X, C and M can occur multiple times in a row (up to 3 times).
-    /// And only one I, X or C can be subtracted (e.g. IV = 4, IX = 9, CD = 400).
-    /// Note: Only the next small number can be subtracted, so IC is not valid.
-    ///
-    /// 99 = XCIX (90+9)
+    /**
+     * Converts the given arabic number to its roman representation.
+     * An Exception is thrown when number is less than 1 or greater than 3888.
+     * 1       I
+     * 5       V
+     * 10      X
+     * 50      L
+     * 100     C
+     * 500     D
+     * 1000    M
+     *
+     * Convert numbers from 1 to 3888.
+     *
+     * Only I, X, C and M can occur multiple times in a row (up to 3 times).
+     * And only one I, X or C can be subtracted (e.g. IV = 4, IX = 9, CD = 400).
+     * Note: Only the next small number can be subtracted, so IC is not valid.
+     *
+     * 99 = XCIX (90+9)
+     * @param int $number
+     * @return string
+     */
     public function toRoman(int $number): string;
 }
