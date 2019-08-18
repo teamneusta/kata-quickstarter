997ebec4ce835f7e4373ae43b19bf6f4713e9868
Ole Rößner <o.roessner@neusta.de>
2019-08-18 13:55:46 +0200

    added description to StringCalculatorInterface::add()

--- php-mocking-kata/src/Calculations/StringCalculatorInterface.php
+++ php-mocking-kata/src/Calculations/StringCalculatorInterface.php
@@ -8,5 +8,12 @@ namespace RealLife\Calculations;
  */
 interface StringCalculatorInterface
 {
+    /**
+     * This is from the famous String Calculator Kata ({@link http://codingdojo.org/kata/StringCalculator}):
+     * 
+     * - The method can take 0, 1 or 2 numbers separated by comma, and returns their sum.
+     * - An empty string will return “0”.
+     * - Example of inputs: "", "1", "1.1,2.2".
+     */
     public function add(string $numbers): int;
 }
